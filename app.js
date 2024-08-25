8e8ccb765157d2a1b77e48fac4236fd069335032
taifur-rahaman <sr.taifur@gmail.com>
2024-08-25 14:15:00 +0600

    html, css and js added

---
+++ app.js
@@ -0,0 +1,29 @@
+const decrease = document.getElementById("decrease");
+const reset = document.getElementById("reset");
+const increase = document.getElementById("increase");
+
+let count = 0;
+
+decrease.addEventListener("click", () => {
+  count--;
+  document.getElementById("counter").innerHTML = count;
+  if (count < 0) {
+    document.getElementById("counter").style.color = "red";
+  }
+});
+
+reset.addEventListener("click", () => {
+  count = 0;
+  document.getElementById("counter").innerHTML = count;
+  if (count == 0) {
+    document.getElementById("counter").style.color = "grey";
+  }
+});
+
+increase.addEventListener("click", () => {
+  count++;
+  document.getElementById("counter").innerHTML = count;
+  if (count > 0) {
+    document.getElementById("counter").style.color = "green";
+  }
+});
